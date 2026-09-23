91c6ab94fd7d4a4b2854861172cdef8e84a8d7d6
sandrasantos1124 <sssandrasantos1124@gmail.com>
2026-09-23 01:20:14 -0400

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+a=int(input('Escribe el primer numero:'))
+b=int(input('Escribe el segundo:'))
+suma=a+b
+resta=a-b
+multi=a*b
+divi=a/b
+print("La suma es:", suma)
+print(f'La resta es:{resta}')
+print("La multiplicación es", multi)
+print(f'La división es:{divi}')
