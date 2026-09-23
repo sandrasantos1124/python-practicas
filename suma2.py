9ffa80f6b5f700e2be443bdf665e28260143c426
sandrasantos1124 <sssandrasantos1124@gmail.com>
2026-09-23 01:27:38 -0400

    Update suma2.py

--- suma2.py
+++ suma2.py
@@ -1,4 +1,10 @@
-a=int(input("Escriba el primer número:"))
-b=int(input("Escriba el segundo número:"))
-suma= a+b
-print("La suma es:",suma
+a=int(input('Escribe el primer numero:'))
+b=int(input('Escribe el segundo:'))
+suma=a+b
+resta=a-b
+multi=a*b
+divi=a/b
+print("La suma es:", suma)
+print(f'La resta es: {resta}')
+print("La multiplicación es: ", multi)
+print(f'La división es: {divi}')
